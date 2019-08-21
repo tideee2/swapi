034aa70ff25d7e4aeab55482cbeea7582e1970f2
d_yermakov <d_yermakov@mifprojects.com>
2019-08-21 15:32:17 +0300

    output 1lvl data

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,25 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../../services/api.service';
-
+import {from, Observable, Observer, of} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {error} from 'util';
+import {finalize, toArray} from 'rxjs/operators';
+const PARTS = environment.parts;
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-  data = [];
+  data: any = [];
+  temp: any = {};
+  nestedObj: any = {};
+  objectKeys = Object.keys;
   constructor(private apiSrv: ApiService) { }
 
   ngOnInit() {
     console.log('t');
-    this.apiSrv.getAllData().subscribe(value => {
-      // console.log(value);
-      value.map(link => link.subscribe(data => console.log(data)));
+    this.apiSrv.getAllData('g')
+      .subscribe(value => {
+        console.log(value);
+        value.map(link => {
+          link.data.subscribe(info => {
+            this.temp[link.key] = info;
+            console.log(this.temp);
+            this.data.push({
+              key: link.key,
+              data: info
+            });
+          });
+        });
+    },
+      err => {
+        console.log(err);
+      },
+      () => {
+        console.log(this.data);
     });
-    // this.apiSrv.getData().subscribe(value => {
-    //   console.log(value);
-    //   // value.subscribe(data => console.log(data));
-    // });
   }
-
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,37 +1,98 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {forkJoin, merge, Observable} from 'rxjs';
+import {BehaviorSubject, forkJoin, from, merge, Observable, of} from 'rxjs';
 import {PeopleModel, PeopleModelQuery} from '../models/people.model';
-import {map, tap, mergeMap, combineAll, toArray} from 'rxjs/operators';
+import {map, tap, mergeMap, combineAll, toArray, flatMap} from 'rxjs/operators';
+import {environment} from '../../environments/environment';
+import {ajax} from 'rxjs/ajax';
+
+const PARTS = environment.parts;
+const paths = environment.paths;
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  subject = new BehaviorSubject(null);
+  subjectTemp = new BehaviorSubject(null);
 
-  constructor(private http: HttpClient) { }
-
-  getData(query?: string): Observable<any> {
-    return this.http.get<any>('https://swapi.co/api/people/?search=ky').pipe(
-        map(value => {
-          return {
-            ...value.results[0], films: value.results[0].films.map(data => {
-              console.log(data);
-              return this.http.get(data).pipe(toArray());
-            })
-          };
-       })
-    );
-  // );
+  subjectArr: any = {};
+  constructor(private http: HttpClient) {
+    PARTS.forEach(el => {
+      // this.subjectArr.push(new BehaviorSubject(null));
+      this.subjectArr[el] = new BehaviorSubject(null);
+    });
+    console.log(this.subjectArr);
   }
+
   getAllData(query: string = 'k'): Observable<any> {
     return this.http.get('https://swapi.co/api/?format=json').pipe(
       map(value => {
         console.log(value);
         return Object.keys(value).map(key => {
-          return this.http.get(`${value[key]}?search=${query}`);
+          return {
+            key,
+            data: this.getPartData(`${value[key]}?search=${query}`)
+          };
+        });
+      }),
+      tap(value => {
+          console.log(value);
+          value[0].data.subscribe(x => {
+            console.log(x.results[0]);
+            x.results[0].films.forEach(film => {
+              film.subscribe(y => {
+                // this.subject.next(y);
+              });
+            });
+          });
+        }),
+      tap (value => {
+        value.forEach(x => {
+          // console.log(x);
+          x.data.subscribe(y => {
+            // console.log(y);
+            y.results.forEach(z => {
+              console.warn(z);
+              for (let i = 0; i < PARTS.length; i++) {
+                if (z[PARTS[i]] && z[PARTS[i]] !== []) {
+                  z[PARTS[i]].forEach(q => {
+                    q.subscribe(w => {
+                      // this.subject.next({
+                      //   key: x.key,
+                      //   type: PARTS[i],
+                      //   owner: z.name || z.title,
+                      //   data: w
+                      // });
+                    });
+                  });
+                }
+              }
+            });
+          });
         });
       })
     );
+  }
+
+  getPartData(link: string) {
+    return this.http.get(link).pipe(
+      map((value: any) => {
+        const x = value.results.map(data => {
+          for (let i = 0; i < PARTS.length; i++) {
+            if (data[PARTS[i]] && data[PARTS[i]] !== []) {
+              data[PARTS[i]] = data[PARTS[i]].map(part => {
+                return this.getNestedData(part);
+              });
+            }
+          }
+          return data;
+        });
+        return value;
+      })
+    );
+  }
+  getNestedData(link: string): Observable<any> {
+    return this.http.get(link);
   }
 }
 
